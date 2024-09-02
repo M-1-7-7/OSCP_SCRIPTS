cd6f1e8372e6c657d1faa924ca604d58b8729c5d
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2024-09-02 19:18:10 +1000

    Update HTTP,HTTPS_web_enum.sh

--- HTTP,HTTPS_web_enum.sh
+++ HTTP,HTTPS_web_enum.sh
@@ -19,34 +19,46 @@ do
 	echo "https://$ip:$line/" >> url.txt
 done;
 
-
-
+domain=$1
+mkdir $domain && cd $domain
+touch url.txt
+echo "http://$domain/" >> url.txt
 run_whois(){
 	echo "--- Executing WHOIS on valid urls ---"
 	touch whois_output.txt
 	cat url.txt | while read line;
  	do
+ 		echo $line
   		whois $line >> whois_output.txt
     	done;
+}
 
 run_curl(){
 	echo "--- Executing CURL on valid urls ---"
 	touch curl_output.txt
 	cat url.txt | while read line;
  	do
+  		echo $line
   		curl -I $line >> curl_output.txt
     	done;
-
+}
 run_hakrawler(){
 	echo "--- Executing HAKRAWLER on valid urls ---"
 	touch hakrawler_output.txt
 	cat url.txt | while read line;
  	do
+  		echo $line
   		echo $line | hakrawler -u >> hakrawler_output.txt
     	done;
 }
 
 run_ferox(){
 	echo "--- Executing FEROXBUSTER on valid urls ---"
+	 echo $line
 	cat url.txt | feroxbuster --stdin -k -s 200 301 302 -d 2 -w "/usr/share/seclists/Discovery/Web-Content/combined_words.txt" -o feroxScan.txt
 }
+
+run_whois
+run_curl
+run_hakrawler
+run_ferox
